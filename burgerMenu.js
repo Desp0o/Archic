ce1e9f0146d0aa66e0513d4a799dda5ed091693f
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2022-07-02 11:42:22 +0400

    js

--- burgerMenu.js
+++ burgerMenu.js
@@ -4,9 +4,10 @@ let darkMenu = document.getElementById('menu_black');
 let xMark = document.getElementById('x_mark');
 let gray = document.getElementById('gray');
 
-window.addEventListener('scroll', ()=>{
 
-    if(window.scrollY > 50){
+window.addEventListener('scroll', ()=>{
+    let scroll = window.scrollY;
+    if(scroll){
     navbar.style.backgroundColor = "white";
     navbar.style.top = "2px"
     }else{
@@ -25,21 +26,10 @@ burger.addEventListener('click', (e)=>{
 xMark.addEventListener('click', ()=>{
     darkMenu.style.right = "-70%";
     gray.style.display = "none"
-    
-    if(window.scrollY > 50){
-        navbar.style.backgroundColor = "white";
-    }else{
-        navbar.style.backgroundColor = "";
-    }
 })
 
 gray.addEventListener('click', () =>{
     darkMenu.style.right = "-70%";
     gray.style.display = "none"
     
-    if(window.scrollY > 50){
-        navbar.style.backgroundColor = "white";
-    }else{
-        navbar.style.backgroundColor = "";
-    }
 })
